c4ad96b21e3c2695812974e311b23b86f76f1495
Mateus <abdallahmateus@gmail.com>
2022-05-25 21:05:09 -0300

    add events in the buttons

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const reviews = [
         'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
     },
     {
-        id: 2,
+        id: 3,
         name: 'Phila Rex',
         job: 'Intern',
         img: 
@@ -26,10 +26,19 @@ const reviews = [
         text: 
         'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
+    {
+        id: 4,
+        name: 'TG Rex',
+        job: 'Boss',
+        img: 
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+        text: 
+        'I am baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry'
+    },
 ];
 
 //select items
-const img = document.getElementById('img');
+const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
@@ -43,6 +52,34 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener('DOMContentLoaded', function(){
-   const item = reviews[currentItem];
-      
+   showPerson();
+});
+
+function showPerson(){
+    const item = reviews[currentItem];
+    img.src = item.img;   
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+prevBtn.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+nextBtn.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem =  reviews.length - 1;
+    }
+    showPerson();
+});
+
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
 });
